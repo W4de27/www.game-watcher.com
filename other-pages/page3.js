c27fe175aaf4b4911442aed882f204112108a162
[ZawYa] W4de <165445753+W4de27@users.noreply.github.com>
2024-09-30 15:43:04 +0100

    Add files via upload

---
+++ other-pages/page3.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").addEventListener("click", function () {
+  document.getElementById("main").style.cssText = "display: flex";
+  document.getElementById("aside").style.cssText = "display: flex";
+  document.getElementById("pages").style.cssText = "display: flex";
+  document.getElementById("btn").style.cssText = "display: none";
+});
